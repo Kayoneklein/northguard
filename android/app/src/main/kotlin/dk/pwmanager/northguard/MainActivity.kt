de7d0b88e60087b51460930a4f050eb9a46fdc6d
Joshua Aghanti <kayoneklein@gmail.com>
2026-02-06 08:21:35 +0100

    android setup

---
+++ android/app/src/main/kotlin/dk/pwmanager/northguard/MainActivity.kt
@@ -0,0 +1,13 @@
+package dk.pwmanager.northguard
+
+import io.flutter.embedding.android.FlutterFragmentActivity
+import android.os.Bundle
+import com.google.android.material.color.DynamicColors
+
+class MainActivity : FlutterFragmentActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        // Apply dynamic colors to the application
+        DynamicColors.applyToActivitiesIfAvailable(application)
+    }
+}
